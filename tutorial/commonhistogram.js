d348c90812dd95af7f2bdc5850ef18830c83dacb
zhangbiaoguang <zhangbiaoguang@gridsum.com>
2016-08-09 17:44:59 +0800

    add grid style for background.

--- tutorial/commonhistogram.js
+++ tutorial/commonhistogram.js
@@ -1,6 +1,6 @@
 function renderHistogram(id, data) {
     var formatCount = d3.format(',.0f');
-    var margin = {top: 10, right: 30, bottom: 30, left: 30},
+    var margin = {top: 10, right: 20, bottom: 30, left: 35},
         width = 360 - margin.left - margin.right,
         height = 250 - margin.top - margin.bottom;
 
@@ -8,9 +8,8 @@ function renderHistogram(id, data) {
     //    .rangeRound([0, width]); //continuous.range(range).interpolate(d3.interpolateRound);
 
 	var x = d3.scaleLinear()
-			.domain([0, d3.max(data.labels, function (d) {return d;})])
-			.range([0, width])
-			.interpolate(d3.interpolateRound);
+			.domain([0, d3.max(data.labels, function (d) {return d;})]) //0在这里会影响x轴的始点
+			.range([0, width]);//.interpolate(d3.interpolateRound);
     
     //var bins = d3.histogram()
     //    .domain(x.domain())
@@ -31,7 +30,11 @@ function renderHistogram(id, data) {
     var y = d3.scaleLinear()
         .domain([0, d3.max(bins, function(d) { return d.length; })])
         .range([height, 0]);
-
+        
+    var maxValue = d3.max(bins, function(d) { return d.length; }) || 500, step = maxValue / 8,
+        ysteps = d3.range(0, maxValue + step, step);
+    var barWidth = x(bins[0].x1) - x(bins[0].x0), maxHeight = d3.max(bins, function(d){return height - y(d.length);});
+        
     var svg = d3.select(id)
         .classed('commonchart-histogram', true)
         .append('svg')
@@ -39,6 +42,26 @@ function renderHistogram(id, data) {
         .attr('height', height + margin.top + margin.bottom)
         .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    //根据bar的生成规则来生成格子
+    svg.selectAll('.vertical-line')
+        .data(bins)
+        .enter().append('line')
+        .attr('x1', function(d) {return x(d.x0) + barWidth;})
+        .attr('y1', function(d) {return 0})
+        .attr('x2', function(d) {return x(d.x0) + barWidth;})
+        .attr('y2', maxHeight)
+        .attr('class', 'vertical-line');
+        
+    svg.selectAll('.horizontal-line')
+        .data(ysteps.filter(function(value){return value > 0;}))
+        .enter().append('line')
+        .attr('x1', 0)
+        .attr('y1', function(d) {return y(d);})
+        .attr('x2', width)
+        .attr('y2', function(d) {return y(d);})
+        .attr('class', 'horizontal-line');
+    
     var bar = svg.selectAll('.bar')
         .data(bins)
         .enter().append('g')
@@ -47,10 +70,11 @@ function renderHistogram(id, data) {
 
     bar.append('rect')
         .attr('x', 1)
-        .attr('width', x(bins[0].x1) - x(bins[0].x0) - 1)
-        .attr('height', function(d) { return height - y(d.length); })
+        .attr('width', barWidth - 1)
+        .attr('height', function(d) {return height - y(d.length);})
 		.attr('title', function (d) {return d.length})
-		.on('mouseenter', function (data, i, group) { //d3.event //The current event, if any. 
+		.on('mouseenter', function (data, i, group) { //d3.event //The current event, if any.
+            //point = d3.mouse(document.getElementById('histogram_bar_con'));//获取相对某容器的坐标
 			var tooltip = d3.select(id + ' .d3-tooltip');
 			if (!tooltip.size()) {
 				tooltip = d3.select(id).append('div').attr('class', 'd3-tooltip');
@@ -75,12 +99,11 @@ function renderHistogram(id, data) {
         .attr('transform', 'translate(0,' + height + ')')
         .call(d3.axisBottom(x).tickValues(data.labels)); //利用axis函数来设置ticks，如axis.tickValues([values]),使用固定范围的ticks
 	//.tickFormat(d3.format(',.2f')) 设置tick的格式
-	
-	var maxValue = d3.max(bins, function(d) { return d.length; }) || 500, step = maxValue / 8;
+
 	svg.append('g')
         .attr('class', 'axis axis--y')
         .attr('transform', 'translate(0, 0)')
-        .call(d3.axisLeft(y).tickValues(d3.range(0, maxValue + step, step))); //利用axis函数来设置ticks，如axis.tickValues([values]),使用固定范围的ticks
+        .call(d3.axisLeft(y).tickValues(ysteps)); //利用axis函数来设置ticks，如axis.tickValues([values]),使用固定范围的ticks
 }
 
 //执行
